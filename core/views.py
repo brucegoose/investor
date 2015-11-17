b8039b5f34d0f40fff778f24a3cce715b0cb1e35
Andrew <andjdroid@gmail.com>
2015-11-17 01:00:26 +0000

    Add analysis to investment detail view

--- core/views.py
+++ core/views.py
@@ -26,25 +26,32 @@ class InvestmentDetailView(DetailView):
   model = Investment
   template_name = "investment/investment_detail.html"
 
+  def get_context_data(self, **kwargs):
+    context = super(InvestmentDetailView, self).get_context_data(**kwargs)
+    investment = Investment.objects.get(id=self.kwargs['pk'])
+    answers = Answer.objects.filter(investment=investment)
+    context['analysis'] = answers
+    return context
+
 class InvestmentUpdateView(UpdateView):
   model = Investment
   template_name = 'investment/investment_form.html'
   fields = ['company', 'market', 'option', 'price', 'volume', 'description']
-  
+
 class InvestmentDeleteView(DeleteView):
   model = Investment
   template_name = 'investment/investment_confirm_delete.html'
   success_url = reverse_lazy('investment_list')
-  
+
 class AnswerCreateView(CreateView):
   model = Answer
   template_name = "answer/answer_form.html"
   fields = ['text']
-  
+
   def get_success_url(self):
     return self.object.investment.get_absolute_url()
-  
+
   def form_valid(self, form):
-    form.instance.user = self.request.user 
+    form.instance.user = self.request.user
     form.instance.investment = Investment.objects.get(id=self.kwargs['pk'])
     return super(AnswerCreateView, self).form_valid(form)
