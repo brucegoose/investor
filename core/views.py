f62df2dbaecde78b9364784291bdeebd50362654
Andrew <andjdroid@gmail.com>
2015-11-17 05:11:54 +0000

    Add analysis and investment to user profiles

--- core/views.py
+++ core/views.py
@@ -132,4 +132,12 @@ class UserDetailView(DetailView):
   slug_field = 'username'
   template_name = 'user/user_detail.html'
   context_object_name = 'user_in_view'
-  
+
+  def get_context_data(self, **kwargs):
+    context = super(UserDetailView, self).get_context_data(**kwargs)
+    user_in_view = User.objects.get(username=self.kwargs['slug'])
+    investments = Investment.objects.filter(user=user_in_view)
+    context['investments'] = investments
+    analysis = Analysis.objects.filter(user=user_in_view)
+    context['analysis'] = analysis
+    return context
